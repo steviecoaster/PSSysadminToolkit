95ea2bf350182f8e3ceea59676e78c4fa3c1c001
VALDINGER <svalding@kent.edu>
2018-07-26 22:08:25 -0400

    Added additional query replacements. Thanks @torbar!

--- Public/New-LMGTFYLink.ps1
+++ Public/New-LMGTFYLink.ps1
@@ -9,7 +9,7 @@ Function New-LMGTFYLink {
     .EXAMPLE
     New-LMGTFY -Query "Why is the sky blue?"
 
-    .EXAMPLE 
+    .EXAMPLE
     New-LMGTFY-Query "14th president of united states" -Clipboard
     #>
 
@@ -22,22 +22,44 @@ Function New-LMGTFYLink {
     [switch]$Clipboard
     )
 
-    If($query[-1] -eq '?'){
+    $iievalue = "0"
 
-        $Query = $Query -replace ('\?','%3F')
+    If ($Explainer.IsPresent) {
+
+        $iievalue = "1"
+
     }
 
-
-
+    #rewrite query special characters for URL
+    $query = $query.Replace("%" , "%25")
+    $query = $query.Replace("@" , "%40")
+    $query = $query.Replace("#" , "%23")
+    $query = $query.Replace("$" , "%24")
+    $query = $query.Replace("^" , "%5E")
+    $query = $query.Replace("&" , "%26")
+    $query = $query.Replace("+" , "%2B")
+    $query = $query.Replace("=" , "%3D")
+    $query = $query.Replace("<" , "%3C")
+    $query = $query.Replace(">" , "%3E")
+    $query = $query.Replace("," , "%2C")
+    $query = $query.Replace("/" , "%2F")
+    $query = $query.Replace("\" , "%5C")
+    $query = $query.Replace("{" , "%7B")
+    $query = $query.Replace("}" , "%7D")
+    $query = $query.Replace("[" , "%5B")
+    $query = $query.Replace("]" , "%5D")
+    $query = $query.Replace("'" , "%27")
+    $query = $query.replace("+" , "%2B")
+    $query = $query.replace(" " , "+")
     $query = $query -replace (' ','+')
+    $url = "http://lmgtfy.com/?iie=" + $iievalue + "&q=$query"
 
-    $uri = "http://lmgtfy.com/?q=$query"
 
-    Write-Output "Generated Link: $uri"
+    Write-Output "Generated Link: $url"
 
     If($Clipboard){
 
-        $uri | Clip.exe
+       Set-Clipboard -Value $url
 
     }
 
