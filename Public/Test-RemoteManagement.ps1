0772457d80b239d116a63197a98ca4ab1169bbce
VALDINGER <svalding@kent.edu>
2018-07-27 21:05:55 -0400

    Commiting my CBH, and  removing $testresults at end of file.

--- Public/Test-RemoteManagement.ps1
+++ Public/Test-RemoteManagement.ps1
@@ -1,7 +1,37 @@
 Function Test-RemoteManagement {
+    <#
+    .SYNOPSIS
+    Test various aspects of remote computer connectivity
+
+    .DESCRIPTION
+    This function can test connectivity to a remote endpoint via Ping, WSMan, and WMI
+
+    .PARAMETER Computername
+    An array of computers you wish to test
+
+    .PARAMETER Ping
+    Switch to test Ping connectivity
+
+    .PARAMETER WSMan
+    Switch to test WSMan connectivity used in PSRemoting
+
+    .PARAMETER WMI
+    Switch to test WMI connectivity
+
+    .EXAMPLE
+    Test-RemoteManagement -Computername PC1 -Ping
+
+    .EXAMPLE
+    Test-RemoteManagement -Computername PC1 -Ping -WMI
+
+    .EXAMPLE
+    Test-RemoteManagement -Computername @((Get-ADComputer -Filter * -Searchbase "OU=PC,DC=test,DC=ad").Name) -Ping
+
+    #>
     [cmdletBinding()]
     Param(
-        [Parameter(Mandatory,Position=0)]
+        [Parameter(Mandatory,Position=0,ValueFromPipeline=$true)]
+        [Alias(name,computer)]
         [string[]]$Computername,
         [Parameter(Mandatory=$false,Position=1)]
         [Switch]$Ping,
@@ -70,6 +100,7 @@ Function Test-RemoteManagement {
     }
 
     return $TestResults
+
 }
 
-$TestResults
+
