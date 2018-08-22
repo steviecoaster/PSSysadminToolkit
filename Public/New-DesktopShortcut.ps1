c0fa505723b81467ea0c2bfb3641fb24de9700c2
VALDINGER <svalding@kent.edu>
2018-08-21 21:33:34 -0400

    Add New-DesktopShortcut function

---
+++ Public/New-DesktopShortcut.ps1
@@ -0,0 +1,42 @@
+Function New-DesktopShortcut {
+    <#
+        .SYNOPSIS
+        Creates a Desktop Shortcut in a User's profile
+
+        .PARAMETER Target
+        [String]
+        The full path of the executable/file to which you are creating a shortcut.
+
+        .PARAMETER ShortcutPath
+        The path name of the new Shortcut.
+
+        .EXAMPLE
+        New-DesktopShortcut -Target "C:\Program Files(x86)\SuperSoftware\SuperCool.exe" -ShortcutPath = "$env:Public\Desktop\SuperCool.lnk"
+    
+    #>
+    [cmdletBinding()]
+    Param(
+        
+        [Parameter(Mandatory, Position = 0)]
+        [String]$Target,
+        [parameter(Mandatory, Position = 1)]
+        [String]$ShortcutPath,
+        [Parameter(Mandatory = $False , Position = 2)]
+        [String]$IconPath
+        
+    )
+
+    $Shell = New-Object -ComObject Wscript.Shell
+    $DesktopShortcut = $Shell.CreateShortcut($ShortcutPath)
+    $DesktopShortcut.TargetPath = $Target
+    $DesktopShortcut.WorkingDirectory = Split-Path -Path $Target
+    
+    If ($IconPath) {
+        
+        $DesktopShortcut.IconLocation($IconPath, 0)    
+    
+    }
+
+    $DesktopShortcut.Save()
+
+}
