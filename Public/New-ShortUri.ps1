c6fbc5752fe6eb0e4bdead86449b61cfd3937c1b
Stephen Valdinger <svalding@kent.edu>
2018-10-23 14:57:09 -0400

    Add New-ShortUri function

---
+++ Public/New-ShortUri.ps1
@@ -0,0 +1,98 @@
+Function New-ShortUri {
+    <#
+    .SYNOPSIS
+    Generate tiny url from a web link
+
+    .DESCRIPTION
+    Take a long, unseemly URI from online, and turn it into a short link for distribution or social media
+
+    .PARAMETER Uri
+    The long URI you wish to shorten
+
+    .PARAMETER RetrieveClipboardUri
+    If you have a URI stored in your clipboard, use this to automatically retrieve and shorten it
+
+    .PARAMETER CopyToClipboard
+    Copy the shortened URI to your clipboard
+
+    .PARAMETER Test
+    Opens the short URI in your default web browser to verify the link works as expected
+
+    .EXAMPLE
+
+    New-ShortUri -Uri https://www.superlongdomainname/with/weird/page/links.htm
+
+    .EXAMPLE
+
+    New-ShortUri -RetrieveClipboardUri
+
+    .EXAMPLE
+
+    New-ShortUri -RetrieveCliboardUri
+
+    .EXAMPLE
+
+    New-ShortURI -Uri https://www.dumbsite.com/randomjunk/pg1/ha.htm -CopyToClipboard
+
+    .EXAMPLE
+
+    New-ShortURI -RetrieveClipboardUri -Test
+
+    .EXAMPLE
+
+    Get-Clipbaord | New-ShortUri
+
+    #>
+
+    [cmdletBinding()]
+    [Alias('ShortUri')]
+    Param(
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [String]
+        $Uri,
+
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [Switch]
+        $RetrieveClipboardURI,
+
+        [Parameter(Position = 2)]
+        [Switch]
+        $CopyToClipboard,
+
+        [Parameter(Position = 3)]
+        [Switch]
+        $TestLink
+
+    )
+
+    Process {
+
+        If ($RetrieveClipboardURI) {
+            Add-Type -AssemblyName System.Windows.Forms
+            $Uri = [System.Windows.Forms.Clipboard]::GetData('System.String')
+        }
+
+        $ShortUri = Invoke-WebRequest -Uri "https://tinyurl.com/api-create.php?url=$Uri" | Select-Object -ExpandProperty Content
+
+        If ($CopyToClipboard) {
+
+            [System.Windows.Forms.Clipboard]::SetData('System.String', $ShortUri)
+        }
+
+        $UriObject = [pscustomobject]@{
+
+            LongUri  = $Uri
+            ShortUri = $ShortUri
+        }
+
+        $UriObject
+
+        If ($TestLink) {
+
+            [System.Diagnostics.Process]::Start("$ShortUri")
+        }
+
+    }
+
+}
+
