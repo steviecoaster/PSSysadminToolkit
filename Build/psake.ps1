ac54fff2777b12cde34c24ec2b57fbdc97145799
VALDINGER <svalding@kent.edu>
2018-11-21 21:47:25 -0500

    Pushing changes.

--- Build/psake.ps1
+++ Build/psake.ps1
@@ -1,27 +1,16 @@
+
 [CmdletBinding()]
 param()
 # Psake makes variables declared here available in other scriptblocks
 # Init some things
 Properties {
     # Find the build folder based on build system
-    $ProjectRoot = $env:BHProjectPath
-    if (-not $ProjectRoot) {
-        $ProjectRoot = Resolve-Path -Path "$PSScriptRoot\.."
-    }
+    $ProjectRoot = Resolve-Path -Path "$PSScriptRoot/.."
 
     $Timestamp = Get-Date -Format "yyyyMMdd-hhmmss"
     $PSVersion = $PSVersionTable.PSVersion
     $TestFile = "TestResults_PS${PSVersion}_${TimeStamp}.xml"
     $Lines = '-' * 70
-
-    $DeploymentParams = @{
-        Path    = "$ProjectRoot"
-        Force   = $true
-        Recurse = $false # We keep psdeploy artifacts, avoid deploying those
-    }
-    if ($ENV:BHCommitMessage -match "!verbose") {
-        $DeploymentParams.Add('Verbose', $true)
-    }
 
     $Continue = @{
         InformationAction = if ($PSBoundParameters['InformationAction']) {
@@ -38,18 +27,9 @@ Task 'Default' -Depends 'Test'
 Task 'Init' {
     Set-Location -Path $ProjectRoot
 
-    $CommitTag = if ($env:APPVEYOR_REPO_TAG_NAME) {
-        $env:APPVEYOR_REPO_TAG_NAME
-    }
-    else {
-        "None"
-    }
-
     Write-Information @Continue @"
 $Lines
-Repository Branch: $env:APPVEYOR_REPO_BRANCH
-Commit Tag: $CommitTag
-
+Repository Branch: $env:BUILD_SOURCEBRANCHNAME ($env:BUILD_SOURCEBRANCH)
 Build System Details:
 "@
     Get-Item 'ENV:BH*'
@@ -65,7 +45,8 @@ STATUS: Testing with PowerShell $PSVersion
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
     # Import the module
-    Import-Module "$ProjectRoot/PSSysadminToolkit.psd1"
+    Copy-Item -Recurse "$ProjectRoot/PSSysadminToolkit" -Destination ($env:PSModulePath -split [System.IO.Path]::PathSeparator)[0]
+    Import-Module 'PSSysadminToolkit'
 
     # Gather test results. Store them in a variable and file
     $PesterParams = @{
@@ -78,19 +59,34 @@ STATUS: Testing with PowerShell $PSVersion
 
     [Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
 
-    # In Appveyor?  Upload our tests!
-    If ($ENV:BHBuildSystem -eq 'AppVeyor') {
-        (New-Object 'System.Net.WebClient').UploadFile(
-            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-            "$ProjectRoot\$TestFile"
-        )
-    }
-
     Remove-Item -Path "$ProjectRoot/$TestFile" -Force -ErrorAction SilentlyContinue
 
     # Failed tests?
     # Need to tell psake or it will proceed to the deployment. Danger!
     if ($TestResults.FailedCount -gt 0) {
         Write-Error "Failed $($TestResults.FailedCount) tests; build failed!"
+    }
+}
+
+Task 'Build' -Depends 'Test' {
+    Write-Information @Continue @"
+$Lines
+"@
+    # Load the module, read the exported functions, update the psd1 FunctionsToExport
+    Set-ModuleFunctions
+
+    # Bump the module version if we didn't already
+    try {
+        $GalleryVersion = Get-NextNugetPackageVersion -Name $env:BHProjectName -ErrorAction Stop
+        $GithubVersion = Get-MetaData -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
+        if ($GalleryVersion -ge $GithubVersion) {
+            Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $GalleryVersion -ErrorAction stop
+        }
+    }
+    catch {
+        Write-Information @Continue @"
+Failed to update version for '$env:BHProjectName': $_.
+Continuing with existing version.
+"@
     }
 }
