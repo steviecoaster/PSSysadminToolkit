58517b019bc00078a2617c284ac4b626612fdcff
VALDINGER <svalding@kent.edu>
2018-12-07 21:56:47 -0500

    Fixup bumping version

--- PSGallery.ps1
+++ PSGallery.ps1
@@ -4,7 +4,13 @@ Param(
     [String]
     $Key
 )
-    
+$Manifest = Import-PowerShellDataFile .\PSSysadminToolkit.psd1 
+[version]$version = $Manifest.ModuleVersion
+# Add one to the build of the version number
+[version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
+# Update the manifest file
+Update-ModuleManifest -Path .\PSSysadminToolkit.psd1 -ModuleVersion $NewVersion
+
     $PublishOptions = @{
         Name = "$PSScriptRoot\PSSysadminToolkit.psd1"
         NuGetApiKey = $Key
