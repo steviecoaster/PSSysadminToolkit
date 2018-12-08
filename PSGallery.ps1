c15e0a7dc6e472f23f9489b4a1691f505556c6db
VALDINGER <svalding@kent.edu>
2018-12-07 22:04:29 -0500

    Well, I think it's working. Cleaning this up a bit.

--- PSGallery.ps1
+++ PSGallery.ps1
@@ -4,18 +4,26 @@ Param(
     [String]
     $Key
 )
-$Manifest = Import-PowerShellDataFile .\PSSysadminToolkit.psd1 
-[version]$version = $Manifest.ModuleVersion
-# Add one to the build of the version number
-[version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
-# Update the manifest file
-Update-ModuleManifest -Path .\PSSysadminToolkit.psd1 -ModuleVersion $NewVersion
+    $Manifest = Import-PowerShellDataFile .\PSSysadminToolkit.psd1 
+    [version]$version = $Manifest.ModuleVersion
+    # Add one to the build of the version number
+    [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
+    # Update the manifest file
+
+    $UpdateOptions = @{
+        Path = .\PSSysadminToolkit.psd1
+        ModuleVersion = $NewVersion
+        Tags = "Sysadmin Administration", "Toolkit"
+        LicenseUri = "https://github.com/steviecoaster/PSSysadminToolkit/blob/Dev/LICENSE"
+    }
+
+    Update-ModuleManifest @UpdateOptions
 
     $PublishOptions = @{
+        
+        
         Name = "$PSScriptRoot\PSSysadminToolkit.psd1"
         NuGetApiKey = $Key
-        LicenseUri = "https://github.com/steviecoaster/PSSysadminToolkit/blob/Dev/LICENSE"
-        Tag = "Sysadmin Administration", "Toolkit"
 
     }
  
