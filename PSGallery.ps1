06c76ffe9bb154d7dc88830411fe70d5e390ddb5
VALDINGER <svalding@kent.edu>
2018-12-07 22:21:39 -0500

    More futzing about.

--- PSGallery.ps1
+++ PSGallery.ps1
@@ -4,14 +4,14 @@ Param(
     [String]
     $Key
 )
-    $Manifest = Import-PowerShellDataFile .\PSSysadminToolkit.psd1 
+    $Manifest = Import-PowerShellDataFile $PSScriptRoot\PSSysadminToolkit.psd1 
     [version]$version = $Manifest.ModuleVersion
     # Add one to the build of the version number
     [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
     # Update the manifest file
 
     $UpdateOptions = @{
-        Path = .\PSSysadminToolkit.psd1
+        Path = "$PSScriptRoot\PSSysadminToolkit.psd1"
         ModuleVersion = $NewVersion
         Tags = "Sysadmin Administration", "Toolkit"
         LicenseUri = "https://github.com/steviecoaster/PSSysadminToolkit/blob/Dev/LICENSE"
@@ -22,7 +22,7 @@ Param(
     $PublishOptions = @{
         
         
-        Name = ".\PSSysadminToolkit.psd1"
+        Name = "$PSScriptRoot\PSSysadminToolkit.psd1"
         NuGetApiKey = $Key
 
     }
--- RunTests.ps1
+++ RunTests.ps1
@@ -1,4 +1,4 @@
-$Tests = Get-ChildItem -Path . -Recurse | Where { $_.Extension -eq '.ps1'}
+$Tests = Get-ChildItem -Path .\Test -Recurse | Where { $_.Extension -eq '.ps1'}
 
 $Tests | ForEach-Object {
 
